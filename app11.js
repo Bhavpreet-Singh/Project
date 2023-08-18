40e25bbf5f2467a0c08fa99d17a0b926fe30cd26
Bhavpreet-Singh <138706160+Bhavpreet-Singh@users.noreply.github.com>
2023-08-18 15:47:27 -0400

    Create app11.js

---
+++ app11.js
@@ -0,0 +1,56 @@
+function changeImage() {
+    var image = document.getElementById('bar');
+    if (image.src.match("strawnutri")) {
+        image.src = "watermelon.jpg";
+    }
+    else{
+        image.src = "strawberry..jpg";
+    }
+}
+
+function changeImage1() {
+    var image = document.getElementById('bar1');
+    if (image.src.match("impactnutri")) {
+        image.src = "impact protein.jpg";
+    }
+    else{
+        image.src = "impactnutri.jpg";
+    }
+}
+
+function changeImage2() {
+    var image = document.getElementById('bar2');
+    if (image.src.match("questnutri")) {
+        image.src = "quest bar.jpg";
+    }
+    else{
+        image.src = "questnutri.jpg";
+    }
+}
+function changeImage3() {
+    var image = document.getElementById('straw');
+    if (image.src.match("strawnutri")) {
+        image.src = "strawberry.jpeg";
+    }
+    else{
+        image.src = "strawnutri.jpg";
+    }
+}
+function changeImage4() {
+    var image = document.getElementById('pearl');
+    if (image.src.match("pearlnutri")) {
+        image.src = "pearls.jpg";
+    }
+    else{
+        image.src = "pearlnutri.jpg";
+    }
+}
+function changeImage5() {
+    var image = document.getElementById('apple');
+    if (image.src.match("applenutri")) {
+        image.src = "apple.jpg";
+    }
+    else{
+        image.src = "applenutri.jpg";
+    }
+}
